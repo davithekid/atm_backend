71ff6ed9f56a219f035aa88bab1a98b077678262
davithekid <byjdrk.27@gmail.com>
2024-11-02 01:30:33 -0300

    add switch case

--- index.php
+++ index.php
@@ -1,24 +1,47 @@
+<?php
+session_start();
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CAIXA-ELETRONICO</title>
+    <title>Banco FLacko</title>
 </head>
-
 <body>
-    <button>
-        <a href="./saldo.php">Saldo</a>
-    </button>
-    <button>
-        <a href="./saque.php">Saque</a>
-    </button>
-    <button>
-        <a href="./deposito.php">Depósito</a>
-    </button>
-    <button>
-        <a href="./extrato">Emissão de extrato</a>
-    </button>
-</body>
+    <header>
+    <?php
 
+
+if (isset($_SESSION["saldo"])) { // verificando se saldo ja foi definida ou é um valor null
+   echo '<h1>Saldo da conta: R$' .  $_SESSION["saldo"] . ",00</h1>" ; // imprimir o novo valor de saldo (incrementação do depósito)        
+ } 
+ else {
+   echo "Saldo da conta: R$ 0,00"; // saldo por padrão (antes de depósitar)
+}
+?>
+    </header>
+<main>
+        <h1>Escolha um serviço</h1>
+        
+        <button>
+            <a href="./saque.php">Saque</a>
+        </button>
+        <button>
+            <a href="./deposito.php">Depósito</a>
+        </button>
+        <button>
+            <a href="./extrato">Historico</a>
+        </button>
+    </main>
+
+    <br>
+<button>
+    <a href="./index.php">voltar</a>
+
+
+    </form>
+</body>
 </html>
--- saque.php
+++ saque.php
@@ -1,94 +1,83 @@
 <?php
 session_start();
 
-if (isset($_SESSION['saldo'])){
-    echo "Valor disponivel para saque: " . $_SESSION['saldo'];
+if (isset($_SESSION['saldo'])) {
+    echo "Saldo da conta: R$" . $_SESSION['saldo'] . ",00";
+} else {
+    echo "Saldo da conta: R$0,00";
 }
-else{
-    echo "Valor disponivel para saque: R$0,00";
-}
+
 
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Saque</title>
 </head>
+
 <body>
     <form action="saque.php" method="post">
-   Quanto deseja sacar? <input type="text" name="saque">
-    <input type="submit" value="sacar">
+        Escolha a quantidade que você deseja sacar dessa conta. <input type="text" name="saque">
+        <input type="submit" value="sacar">
     </form>
     <?php
-    $qtd100 = 0; $qtd50 = 0; $qtd20 = 0; $qtd10 = 0; $qtd5 = 0; $qtd2 =0;
-    $resto;
 
 
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
         $saque = $_POST['saque'];
-        if($saque > $_SESSION['saldo']){
-            echo "impossivel efetuar o saque desejado";
-        }else {
-          
-            $valor = $saque;
-            echo "Saque de R$" .  $valor . ",00 efetuado com sucesso!! <br>";
-            $_SESSION['saldo'] -= $saque;
-            
-            if($saque >= 100){
-                $qtd100 = $saque / 100; // atribuindo a quantidade de cédulas
-                $cedulas = ( int )   $qtd100; // transformando em um tipo inteiro
-                $saque = $saque % 100; // armazenando o resto
-                $_SESSION['saldo'] =- $saque; // saldo - saque
-                echo "Você irá receber $cedulas cédula(s) de R$100,00 <br>";     
-            }
 
-            if($saque >= 50){
-                $qtd50 = $saque / 50;
-                $cedulas = ( int ) $qtd50;
-                $saque = $saque %50;
-                $_SESSION['saldo'] =- $saque;
-                echo "Você irá receber $cedulas cédula(s) de R$50,00 <br>";         
+
+        if ($saque > $_SESSION['saldo']) {
+            echo "Valor maior que saldo";
+        } else {
+
+
+            switch ($saque) {
+                case 5:
+                    $saque = 5;
+                    $_SESSION['saldo'] -= $saque;
+                    echo $_SESSION['saldo'];
+                    break;
+                case 10:
+                    $saque = 10;
+                    $_SESSION['saldo'] -= $saque;
+                    echo $_SESSION['saldo'];
+                    break;
+                case 20:
+                    $saque = 20;
+                    $_SESSION['saldo'] -= $saque;
+                    echo $_SESSION['saldo'];
+                    break;
+                case 50;
+                    $saque = 50;
+                    $_SESSION['saldo'] -= $saque;
+                    echo $_SESSION['saldo'];
+                    break;
+                case 100;
+                    $saque = 100;
+                    $_SESSION['saldo'] -= $saque;
+                    echo "Saque de R$$saque,00 efetuado com sucesso!! <br>";
+                    echo "Saldo Atual: R$" . $_SESSION['saldo'] . ",00";
+                    break;
+                case $saque:
+                    break;
+
             }
-            if($saque >= 20){
-                $qtd20 = $saque / 20;
-                $cedulas = ( int ) $qtd20;
-                $saque = $saque % 20;
-                $_SESSION['saldo'] =- $saque;
-                echo "Você irá receber $cedulas cédula(s) de R$20,00 <br>";         
-            }
-            if($saque >= 10){
-                $qtd10 = $saque / 10;
-                $cedulas = ( int ) $qtd10;
-                $saque = $saque % 10;
-                $_SESSION['saldo'] =- $saque;
-                echo "Você irá receber $cedulas cédula(s) de R$10,00 <br>";         
-            }
-            if($saque >= 5){
-                $qtd5 = $saque / 5;
-                $cedulas = ( int ) $qtd5;
-                $saque = $saque % 5;
-                $_SESSION['saldo'] =- $saque;
-                echo "Você irá receber $cedulas cédula(s) de R$5,00 <br>";         
-            }
-            if($saque >= 2){
-                $qtd2 = $saque / 2;
-                $cedulas = ( int ) $qtd2;
-                $saque = $saque % 2;
-                $resto = $saque % 2;
-                $_SESSION['saldo'] =- $saque;
-                echo "Você irá receber $cedulas cédula(s) de R$2,00 <br>";  
-            
-            }
-            
-        } 
+        }
 
     }
 
+
+
     ?>
+    <br>
     <button>
         <a href="./index.php">voltar</a>
     </button>
 </body>
+
 </html>
